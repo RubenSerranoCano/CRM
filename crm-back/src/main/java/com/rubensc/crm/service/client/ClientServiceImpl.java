eee27843da4e9d17d48d3d8c2d16e45cfb3cf646
RubenSerrano <rubenserranocano@gmail.com>
2022-11-29 10:32:28 +0100

    back-feat: (TDD) client status tests (passed)

--- crm-back/src/main/java/com/rubensc/crm/service/client/ClientServiceImpl.java
+++ crm-back/src/main/java/com/rubensc/crm/service/client/ClientServiceImpl.java
@@ -1,13 +1,23 @@
 package com.rubensc.crm.service.client;
 
 import com.rubensc.crm.persistence.model.client.Client;
+import com.rubensc.crm.persistence.repository.client.ClientRepository;
+import com.rubensc.crm.service.client.exceptions.ClientMissingStatusException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ClientServiceImpl implements ClientService {
 
+    @Autowired
+    ClientRepository clientRepository;
+
     @Override
     public Client addClient(Client newClient) {
-        return null;
+
+        if (newClient.getStatusType() == null)
+            throw new ClientMissingStatusException();
+
+        return clientRepository.save(newClient);
     }
 }
--- crm-back/src/main/java/com/rubensc/crm/service/client/exceptions/ClientMissingStatusException.java
+++ crm-back/src/main/java/com/rubensc/crm/service/client/exceptions/ClientMissingStatusException.java
@@ -2,7 +2,7 @@ package com.rubensc.crm.service.client.exceptions;
 
 public class ClientMissingStatusException extends RuntimeException{
 
-    public ClientMissingStatusException(String message){
-        super(message);
+    public ClientMissingStatusException(){
+        super("Client status is missing.");
     };
 }
