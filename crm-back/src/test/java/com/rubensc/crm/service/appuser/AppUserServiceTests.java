949a85404e4131474416e3b59db731d1e707d037
RubenSerrano <rubenserranocano@gmail.com>
2022-11-30 13:04:46 +0100

    back-feat: (TDD) signup encondes password test (failed)

--- crm-back/src/test/java/com/rubensc/crm/service/appuser/AppUserServiceTests.java
+++ crm-back/src/test/java/com/rubensc/crm/service/appuser/AppUserServiceTests.java
@@ -1,9 +1,11 @@
 package com.rubensc.crm.service.appuser;
 
+import com.rubensc.crm.persistence.model.appuser.AppUser;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 public class AppUserServiceTests {
@@ -18,6 +20,14 @@ public class AppUserServiceTests {
     @Test
     void userGetsLoginResponse() {
         Assertions.assertNotNull(appUserService.login("test@email.com", "X5#$Y3hRzkH1"));
+    }
+
+    @Test
+    void signupEncodesPassword() {
+        String rawPassword = "X5#$Y3hRzkH1";
+        AppUser appUserResponse = appUserService.signup("test@email.com", rawPassword).getBody();
+
+        Assertions.assertNotEquals(rawPassword, appUserResponse.getPassword());
     }
 
 }
