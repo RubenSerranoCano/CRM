acfb31e8912fad8c52bb6b0c34b1ec5b40cb49f7
RubenSerrano <rubenserranocano@gmail.com>
2022-11-30 13:34:47 +0100

    add UserDto as data transfer object

--- crm-back/src/main/java/com/rubensc/crm/presentation/appuser/AppUserController.java
+++ crm-back/src/main/java/com/rubensc/crm/presentation/appuser/AppUserController.java
@@ -1,6 +1,7 @@
 package com.rubensc.crm.presentation.appuser;
 
 import com.rubensc.crm.persistence.model.appuser.AppUser;
+import com.rubensc.crm.presentation.appuser.dto.UserDto;
 import com.rubensc.crm.service.appuser.AppUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,20 +11,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("api/v1")
+@RequestMapping("/api/v1")
 public class AppUserController {
 
     @Autowired
     AppUserService appUserService;
 
-    @PostMapping("login")
-    ResponseEntity<AppUser> login(@RequestBody AppUser appUser) {
-        return appUserService.login(appUser.getEmail(), appUser.getPassword());
+    @PostMapping(path = "/login")
+    ResponseEntity<AppUser> login(@RequestBody UserDto userDto) {
+        return appUserService.login(userDto.getEmail(), userDto.getPassword());
     }
 
-    @PostMapping("signup")
-    ResponseEntity<AppUser> signup(@RequestBody AppUser appUser) {
-        return appUserService.signup(appUser.getEmail(), appUser.getPassword());
+    @PostMapping(path = "/signup")
+    ResponseEntity<AppUser> signup(@RequestBody UserDto userDto) {
+        return appUserService.signup(userDto.getEmail(), userDto.getPassword());
     }
 
 }
---
+++ crm-back/src/main/java/com/rubensc/crm/presentation/appuser/dto/UserDto.java
@@ -0,0 +1,13 @@
+package com.rubensc.crm.presentation.appuser.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class UserDto {
+    String email;
+    String password;
+}
