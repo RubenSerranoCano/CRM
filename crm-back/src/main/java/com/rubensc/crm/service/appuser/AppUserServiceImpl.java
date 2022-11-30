af2e8f279f7a236b811cbb8fd16a27b87e653591
RubenSerrano <rubenserranocano@gmail.com>
2022-11-30 12:59:17 +0100

    back-feat: (TDD) user gets response on login (passed)

--- crm-back/src/main/java/com/rubensc/crm/service/appuser/AppUserServiceImpl.java
+++ crm-back/src/main/java/com/rubensc/crm/service/appuser/AppUserServiceImpl.java
@@ -1,14 +1,35 @@
 package com.rubensc.crm.service.appuser;
 
 import com.rubensc.crm.persistence.model.appuser.AppUser;
+import com.rubensc.crm.persistence.repository.appuser.AppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
-public class AppUserServiceImpl implements AppUserService{
+public class AppUserServiceImpl implements AppUserService {
+
+    @Autowired
+    AppUserRepository appUserRepository;
+
     @Override
     public ResponseEntity<AppUser> login(String email, String rawPassword) {
-        return null;
+        Optional<AppUser> optionalAppUser = appUserRepository.findByEmail(email);
+
+        if (optionalAppUser.isPresent()) {
+            AppUser appUser = optionalAppUser.get();
+
+            if (appUser.getPassword().equals(rawPassword)) {
+                return ResponseEntity.status(HttpStatus.OK).body(appUser);
+            } else {
+                return ResponseEntity.status(HttpStatus.FORBIDDEN).body(null);
+            }
+
+        }
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(null);
     }
 
     @Override
